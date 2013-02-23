bf778e13aaad139947f5984d4742f3c4e520c995
sashamua <sasham@gmx.co.uk>
2013-02-23 15:06:26 +0000

    Application framework added (Drupal 7.20)

---
+++ profiles/usergov/install.bat
@@ -0,0 +1,5 @@
+@echo Reseting IIS...
+@iisreset
+@cd C:\inetpub\myweb
+@IF EXIST sites/default/settings.php @ATTRIB -R sites/default/settings.php
+@drush -y si usergov --db-url=mysql://usergov_admin:Password1234@localhost:3306/usergov --account-name=admin --account-mail=meloks@yahoo.com --account-pass=Password1234 --site-mail="admin@company.com" --site-name="UserGov" install_configure_form.site_default_country=GB install_configure_form.date_default_timezone="Europe/London"
