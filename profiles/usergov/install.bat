e86b90d107586d3cdc24efc113ed3a35a064d844
sashamua <sasham@gmx.co.uk>
2013-03-05 13:30:04 +0000

    More installation changes

--- profiles/usergov/install.bat
+++ profiles/usergov/install.bat
@@ -2,4 +2,4 @@
 @iisreset
 @cd C:\inetpub\myweb
 @IF EXIST sites/default/settings.php @ATTRIB -R sites/default/settings.php
-@drush -y si usergov --db-url=mysql://app_user:M00nshine@localhost:3306/myweb --account-name=admin --account-mail=meloks@yahoo.com --account-pass=Password1234 --site-mail="admin@company.com" --site-name="UserGov" install_configure_form.site_default_country=GB install_configure_form.date_default_timezone="Europe/London"
+@drush -y si usergov --db-url=mysql://app_user:Password1234@localhost:3306/myweb --account-name=admin --account-mail=meloks@yahoo.com --account-pass=Password1234 --site-mail="admin@company.com" --site-name="UserGov" install_configure_form.site_default_country=GB install_configure_form.date_default_timezone="Europe/London"
